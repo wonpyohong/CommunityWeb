6253b40597fd6e8eeb33b57b1dea5001b1e14ad7
Wonpyo Hong <deblan2@naver.com>
2019-03-27 18:56:00 +0900

    코틀린 새 프로젝트

---
+++ src/main/kotlin/com/example/communityweb/CommunitywebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.communityweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunitywebApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommunitywebApplication>(*args)
+}
