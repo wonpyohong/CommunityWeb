fce513aec4a24a151df02416511d0c33e4660900
Wonpyo Hong <deblan2@naver.com>
2019-03-28 19:41:16 +0900

    오타 수정

--- src/main/kotlin/com/web/domain/Board.kt
+++ src/main/kotlin/com/web/domain/Board.kt
@@ -35,8 +35,7 @@ data class Board(
 
         @OneToOne(fetch = FetchType.LAZY)
     // 실제 DB에는 User의 PK인 user_idx가 저장된다.
-    // eager와 lazy 두 종류가 있는데, eager는 Board 도메인(?)
-    // 조회 즉시 User 객체 조회,
-    // lazy는 User 객체가 실제로 사용될 때 (User 객체 사용 시점 아님)
+    // eager와 lazy 두 종류가 있는데, eager는 Board 도메인(?) 조회 즉시 User 객체 조회,
+    // lazy는 User 객체가 실제로 사용될 때 (User 객체 조회 시점 아님)
     val user: User? = null
 ): Serializable
